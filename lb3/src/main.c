f8fa8f3a56da55339097be5ec708cfc42892c7b2
Alexey R <lexa260011@ngs.ru>
2017-04-19 22:28:47 +0000

    metod

--- lb3/src/main.c
+++ lb3/src/main.c
@@ -9,80 +9,91 @@ double intrvl = 0.5;
 
 double f(double x)
 {
-    return x * x - 4;
+  return x * x - 4;
 }
 
 double f1(double x)
 {
-    return 2 * x;
+  return 2 * x;
 }
 
 void bisec()
 {
-    assert(f(a) * f(b) < 0);
+  assert(f(a) * f(b) < 0);
 
-    int i;
-    for (i = 0; fabs(b - a) > e && f(c) != 0; i++) {
-        c = (a + b) / 2;
-        if (f(a) * f(c) < 0)
-            b = c;
-        else
-            a = c;
-    }
-    printf("bisec steps: %d\n", i);
+  int i;
+  for (i = 0; fabs(b - a) > e && f(c) != 0; i++) {
+    c = (a + b) / 2;
+    if (f(a) * f(c) < 0)
+      b = c;
+    else
+      a = c;
+  }
+  printf("bisec steps: %d\n", i);
 }
 
 void hord()
 {
-    double tmp = 0;
-    int i, flg = 0;
-    for (i = 0; (fabs(c - tmp) > e || !flg) && f(c) != 0; i++) {
-        flg++;
-        tmp = c;
-        c = a - f(a) * (b - a) / (f(b) - f(a));
-        if (f(a) * f(c) < 0)
-            b = c;
-        else
-            a = c;
-    } 
-    printf("hord steps: %d\n", i);
+  double tmp = 0;
+  int i, flg = 0;
+  for (i = 0; (fabs(c - tmp) > e || !flg) && f(c) != 0; i++) {
+    flg++;
+    tmp = c;
+    c = a - f(a) * (b - a) / (f(b) - f(a));
+    if (f(a) * f(c) < 0)
+      b = c;
+    else
+      a = c;
+  } 
+  printf("hord steps: %d\n", i);
 }
 
 void newtoon()
 {
-    double tmp = a;
-    int j;
-    c = (-1) * (f(tmp) / f1(tmp)) + tmp;
-    for (j = 0; f(c) != 0 && fabs(c - tmp) > e; j++) {
-        tmp = c;
-        c = tmp - (f(tmp) / f1(tmp));
-    } 
-    printf("newtoon steps: %d\n", j);
+  double tmp = a;
+  int j;
+  c = (-1) * (f(tmp) / f1(tmp)) + tmp;
+  for (j = 0; f(c) != 0 && fabs(c - tmp) > e; j++) {
+    tmp = c;
+     c = tmp - (f(tmp) / f1(tmp));
+  } 
+  printf("newtoon steps: %d\n", j);
 }
 
 int main(int argc, char **argv)
 {
-    assert(!(argc < 3));
-   
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
+  assert(!(argc < 3));
+  FILE* dat = fopen("metod.txt", "r");
+  int d[3];
+  a = atoi(argv[1]);
+  b = atoi(argv[2]);
+  int i = 0;
+  for (i = 0; i < 3; i++) {
+    fscanf(dat, "%d", &d[i]);
+  }
 
-    int tmpa = a;
-    int tmpb = b;
+  int tmpa = a;
+  int tmpb = b;
+  if (d[0]) {
     bisec();
     printf("answ: %.2lf\n", c);
-
     c = 0;
     a = tmpa;
     b = tmpb;
+  }
+
+  if (d[1]) {
     hord();
     printf("answ: %.2lf\n", c);
-
     c = 0;
     a = tmpa;
     b = tmpb;
+  }
+
+  if (d[2]) {
     newtoon();
     printf("answ: %.2lf\n", c);
+  }
 
     return 0;
 }
