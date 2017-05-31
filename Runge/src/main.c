252c9411f2589a68ab5f2914558ecc5b6e375757
Alexey R <lexa260011@ngs.ru>
2017-05-31 21:34:41 +0700

    Runge

--- Runge/src/main.c
+++ Runge/src/main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
   int n = (b - a) / h + 1;
   int iter = 0;
   int flg = 0;
-
+  double hEiler, hRunge2;
   n = (b - a) / h + 1;
   double tmpy = y0;
   double tmpy1 = y0;
@@ -60,15 +60,42 @@ int main(int argc, char **argv)
       h = h / 2;
       if (b1 <= eps) break;
   } while (1);
-  iter = 0;
+  hEiler = h1;
+		/*---------------------*/
+  
+  h = tmph;
+  n = (b - a) / h + 1;
+  tmpy = y0;
   tmpy1 = y0;
-  printf("x:Eiler func: [%.4lf, %.12lf]\n", a, y0);
-  for (int i = 1; i < n1; i++) {
-    tmpy1 = tmpy1 + h1 * func( a + h1*(i-1), tmpy1);
-    printf("4:Eiler func: [%.4lf, %.12lf][%lf], iter = %d\n", a + h1*i, tmpy1, h1, ++iter);
+  double tmp;
+  
+  printf("x:Runge2 func: [%.4lf, %.12lf]\n", a, y0);
+  do {
+  n = (b - a) / h + 1;
+  tmpy = y0;
+  tmpy1 = y0;
+  iter = 0;
+  for (int i = 1; i < n; i++) {
+    tmp = tmpy + h * func( a + h*(i-1), tmpy);
+    tmpy = tmpy + (h / 2) * (func( a + h*(i-1), tmpy) + func( a + h*i, tmp));
+    printf("1:Runge2 func: [%.4lf, %.12lf][%lf], iter = %d\n", a + h*i, tmpy, h, ++iter);
   }
-	
-	
+    printf("-----------------------------------\n");
+      double a1 = tmp;
+      double a2 = tmpy;
+      double b1 = a1 - a2;
+
+      if (b1 < 0) {
+        b1 = -b1;
+      }
+      printf("3:Runge2 func: [%.12lf]\n",b1);
+    printf("-----------------------------------\n");
+      h = h / 2;
+      if (b1 <= eps) break;
+  } while (1);
+  hRunge2 = h1;
+  
+		/*---------------------*/
   printf("\n\n");
   tmpy = y0;
   tmpy1 = y0;
@@ -110,8 +137,30 @@ int main(int argc, char **argv)
       h = h / 2;
       if (b1 <= eps) break;
   } while (1);
+  
+  printf("--------------ANSWER---------------\n\n");
+  
+  iter = 0;
+  tmpy1 = y0;
+  n1 = (b - a) / hEiler + 1;
+  printf("x:Eiler func: [%.4lf, %.12lf]\n", a, y0);
+  for (int i = 1; i < n1; i++) {
+    tmpy1 = tmpy1 + hEiler * func( a + hEiler*(i-1), tmpy1);
+    printf("4:Eiler func: [%.4lf, %.12lf][%lf], iter = %d\n", a + h1*i, tmpy1, hEiler, ++iter);
+  }
+    printf("-----------------------------------\n\n");
+  iter = 0;
+  tmpy1 = y0;
+  n1 = (b - a) /hRunge2 + 1;
+  printf("x:Runge2 func: [%.4lf, %.12lf]\n", a, y0);
+  for (int i = 1; i < n1; i++) {
+    tmpy1 = tmpy1 + hRunge2 * func( a +hRunge2*(i-1), tmpy1);
+    printf("4:Runge2 func: [%.4lf, %.12lf][%lf], iter = %d\n", a + hRunge2*i, tmpy1, hRunge2, ++iter);
+  }
+    printf("-----------------------------------\n\n");
   printf("x:Runge4 func: [%.4lf, %.12lf]\n", a, y0);
   tmpy1 = y0;
+  n1 = (b - a) / h1 + 1;
   for (int i = 1; i < n1; i++) {
     double k11 = func(a + h1*(i-1), tmpy1);
     double k21 = func(a + h1*(i-1) + h1 / 2, tmpy1 + h1 / 2 * k11);
